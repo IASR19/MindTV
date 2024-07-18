eb0744423024b2c7c176f59e3772c19b4734d960
IGSR19 <itamar_augusto@ges.inatel.br>
2024-07-18 14:53:25 -0300

    Add eeg data collect :sparkle:

---
+++ collect_eeg_data.py
@@ -0,0 +1,23 @@
+import psycopg2
+from pyOpenBCI import OpenBCICyton
+
+def save_eeg_data(participant_id, eeg_data):
+    conn = psycopg2.connect(dbname='neurodata', user='postgres', password='yourpassword', host='localhost')
+    cursor = conn.cursor()
+    
+    for timestamp, channels in eeg_data:
+        cursor.execute("INSERT INTO eeg_data (participant_id, timestamp, channel1, channel2) VALUES (%s, %s, %s, %s)",
+                       (participant_id, timestamp, channels[0], channels[1]))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def eeg_callback(sample):
+    timestamp = sample.id  # or any method to get current timestamp
+    channels = sample.channels_data
+    save_eeg_data(participant_id, [(timestamp, channels)])
+
+board = OpenBCICyton(port='/dev/ttyUSB0', daisy=True)
+participant_id = 1  # ID do participante
+board.start_stream(eeg_callback)
